4ff42ca6b5179aab7af036c34c6ed1106568ebd9
kevinsalazar <kevinlsalazar@yape.com.pe>
2025-09-20 20:30:54 -0500

    eat:

--- src/pages/karaoke/shared/components/bottom-navigation.tsx
+++ src/pages/karaoke/shared/components/bottom-navigation.tsx
@@ -55,7 +55,8 @@ export const BottomNavigation: FC<BottomNavigationProps> = ({
       {/* <div className="absolute inset-0 bg-black/60 backdrop-blur-md" /> */}
 
       {/* Navigation container */}
-      <div className="relative bg-yellow rounded-t-2xl mx-4 mb-2 border-t border-gray-700">
+        <div className="flex items-center justify-around py-2 px-4">
+      <div className="relative bg-black/80  rounded-t-2xl mx-4 mb-2 border-t border-gray-700">
         <div className="flex items-center justify-around py-2 px-4">
           {tabs.map((tab) => {
             const IconComponent = tab.icon;
