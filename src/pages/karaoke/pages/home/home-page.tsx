6ec13257185e5499315fed6830ffb2766387e20a
kevinsalazar <kevinlsalazar@yape.com.pe>
2025-09-20 20:07:06 -0500

    Feat: removed useeffecto

--- src/pages/karaoke/pages/home/home-page.tsx
+++ src/pages/karaoke/pages/home/home-page.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { KaraokeColors } from "../../colors";
 import { HeaderScreen, BottomNavigation } from "../../shared/components";
